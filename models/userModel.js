2e3980a34a1ee07231faa186f3ec16dac7a95dfd
Abdul-Basit Adebayo <abdulbazit@yahoo.ca>
2021-01-24 08:50:12 +0100

    user mods

--- models/userModel.js
+++ models/userModel.js
@@ -68,33 +68,27 @@ userSchema.pre('save', async function(next) {
     this.password = await bcrypt.hash(this.password, salt)
 });
 
-
-
 //Generate and hash password token
-UserSchema.methods.getResetPasswordToken = function () {
+userSchema.methods.getResetPasswordToken = function() {
     // Generate token
- 
+
     const resetToken = crypto.randomBytes(20).toString('hex');
- 
+
     // Hash token and set reset to resetPasswordToken field
     this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
- 
+
     //Set expire
     this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
- 
+
     return resetToken;
- };
+};
 
-
-
-UserSchema.methods.getSignedJwtToken = function() {
+userSchema.methods.getSignedJwtToken = function() {
 
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     })
-  }
-  
-
+}
 
 const User = mongoose.model('User', userSchema)
 
